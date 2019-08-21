46afa1b5b98fbd84512899a07168b140414500d9
Fredrik Bengtsson <fredrik.bengtsson94@gmail.com>
2019-08-21 09:54:06 +0200

    server: add bonus-rule and module docs to rules

--- server_application/rules.py
+++ server_application/rules.py
@@ -2,6 +2,12 @@ import random
 from typing import Optional, List
 from collections import Counter
 
+"""
+    Module implementing the different rules of Yatzy.
+    Most of these depends on a set of dice values to determine the score.
+    The rules are based on: https://en.wikipedia.org/wiki/Yatzy
+"""
+
 def lower_section(dice: List[int], number:int) -> Optional[int]:
     return dice.count(number)*number if dice.count(number)*number != 0 else None
 
@@ -60,3 +66,10 @@ def full_house(dice: List[int]) -> Optional[int]:
 
 def chance(dice: List[int]) -> int:
     return sum(dice)
+
+def bonus(upper_section_score: int) -> Optional[int]:
+    """
+        This rule is special as it depends on the total score of the upper section
+        rather than on any dice.
+    """
+    return 50 if upper_section_score >= 63 else None
